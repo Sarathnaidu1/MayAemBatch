a9b9ea0d89fe433dcb6f184bb10b1523b5c3ba46
Sarathnaidu1 <sarathnaidu513@gmail.com>
2025-06-01 23:40:49 +0530

    MayAemBatch

---
+++ core/src/main/java/com/adobe/aem/services/Demo.java
@@ -0,0 +1,29 @@
+package com.adobe.aem.services;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component(immediate = true, enabled = true, name = "DemoService")
+public class Demo {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Demo.class);
+
+    @Activate
+    public void activateContent() {
+        LOG.info("msg coming from the activate()");
+    }
+
+    @Deactivate
+    public void deactivate() {
+        LOG.info("msg coming from the deactivate()");
+    }
+
+    @Modified
+    public void update() {
+        LOG.info("msg coming from the modified()");
+    }
+}
