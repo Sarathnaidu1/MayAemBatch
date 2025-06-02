f6617e9592a653414055b19a4561142ed80519c5
Sarathnaidu1 <sarathnaidu513@gmail.com>
2025-06-02 15:37:02 +0530

    AemMaybatch

--- core/src/main/java/com/adobe/aem/guides/wknd/core/services/Demo.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/services/Demo.java
@@ -1,4 +1,4 @@
-package com.adobe.aem.services;
+package com.adobe.aem.guides.wknd.core.services;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -7,23 +7,20 @@ import org.osgi.service.component.annotations.Modified;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Component(immediate = true, enabled = true, name = "DemoService")
+@Component(immediate = true,enabled = true,name = "DemoService")
 public class Demo {
-
-    private static final Logger LOG = LoggerFactory.getLogger(Demo.class);
-
+    private static final Logger LOG=LoggerFactory.getLogger(Demo.class);
     @Activate
-    public void activateContent() {
-        LOG.info("msg coming from the activate()");
+    public void activateContent(){
+       LOG.info("msg coming from the activate()");
     }
-
     @Deactivate
-    public void deactivate() {
+    public void DeActivate(){
         LOG.info("msg coming from the deactivate()");
-    }
 
-    @Modified
-    public void update() {
-        LOG.info("msg coming from the modified()");
     }
+@Modified
+public void update(){
+    LOG.info("msg coming from the deactivate()");
+}
 }
