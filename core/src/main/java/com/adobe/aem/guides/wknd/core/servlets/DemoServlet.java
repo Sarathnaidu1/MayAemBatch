7d13f558436875a8822c1ccb4b86c69c307bcda2
Sarathnaidu1 <sarathnaidu513@gmail.com>
2025-06-06 00:51:42 +0530

    AemMayBatch

--- core/src/main/java/com/adobe/aem/guides/wknd/core/servlets/DemoServlet.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/servlets/DemoServlet.java
@@ -7,19 +7,37 @@ import javax.servlet.ServletException;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
-import org.apache.sling.servlets.annotations.SlingServletPaths;
+import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
 import org.osgi.service.component.annotations.Component;
 
 @Component(service = Servlet.class,immediate = true,enabled = true)
-@SlingServletPaths(value = "/bin/demo/surge")
-public class DemoServlet extends SlingSafeMethodsServlet 
+@SlingServletResourceTypes(resourceTypes = "surge/beml/6thcross")
+public class DemoServlet extends SlingAllMethodsServlet
 {
     @Override
     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
             throws ServletException, IOException {
-        response.getWriter().write("data coming from the SlingSafeMethodSErvlet-doGet()");
-    }
-
+        response.getWriter().write("data coming from the SlingSafeMethodSErvlet-doGet");
+            }
+        @Override
+    protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response)
+            throws ServletException, IOException {
+        response.getWriter().write("data coming from the SlingSafeMethodSErvlet-doPost");
+            }
+        @Override
+    protected void doDelete(SlingHttpServletRequest request, SlingHttpServletResponse response)
+            throws ServletException, IOException {
+        response.getWriter().write("data coming from the SlingSafeMethodSErvlet-doDelete");
+            }
+        @Override
+    protected void doPut(SlingHttpServletRequest request, SlingHttpServletResponse response)
+            throws ServletException, IOException {
+        response.getWriter().write("data coming from the SlingSafeMethodSErvlet-doPut");
 }
+}
+
+
+
 
